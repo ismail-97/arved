5abe5d37e2700e67ef8de93e69b7939a40690d70
Ismail Dewidar <ismailkamaleldin@gmail.com>
2022-03-04 12:14:23 +0300

    message

--- utils/config.js
+++ utils/config.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 const MONGODB_URI = process.env.NODE_ENV === 'test' 
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
   
-  console.log("PORT == ", PORT)
 module.exports = {
     MONGODB_URI,
     PORT
