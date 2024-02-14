586f487747c90c348105c0d23a1e79f785bfdc56
ISMDEW <ismailkamaleldin@gmail.com>
2024-02-14 22:11:54 +0300

    updated signup form

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express')
 const app = express()
 require('express-async-errors')
@@ -20,33 +22,34 @@ const mongoose = require('mongoose')
 //database connection
 logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.MONGODB_URI)
-   .then(() => {
-     logger.info('connected to MongoDB')
-   })
-   .catch((error) => {
-     logger.error('error connecting to MongoDB:', error.message)
-   })
-  
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 // implemented before calling route handlers
-app.use(cors(
-  {  exposedHeaders: ['Content-Disposition'] }
-    ))
+app.use(cors({ exposedHeaders: ['Content-Disposition'] }))
 app.use(express.static('build'))
 app.use(express.json())
 app.use(methodOverride('_method'))
 app.use(middleware.requestLogger)
 
 // route handlers
+
 app.use('/', loginRouter)
 app.use('/admin', adminRouter)
 app.use('/user', userRouter)
 app.use('/user/products', productRouter)
+app.get('/*', function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/build/index.html'))
+})
 
 // implemented after calling route handlers
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
-
 
 module.exports = app
--- controllers/loginRouter.js
+++ controllers/loginRouter.js
@@ -18,145 +18,153 @@ const User = require('../models/user')
 const faculty = require('../models/faculty')
 
 // sign up
-loginRouter.post('/register',
-    async (request, response) => {
-        const body = request.body
-        const {password} = body
-        // console.log(body)
-            const saltRounds = 10
-            const passwordHash = await bcrypt.hash(password, saltRounds)
-            const user = new User({
-                ...body,
-                passwordHash: passwordHash,
-                role: 'user',
-                status: 'pending'
-            })
+loginRouter.post('/register', async (request, response) => {
+  const body = request.body
+  const { password } = body
+  // console.log(body)
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+  const user = new User({
+    ...body,
+    passwordHash: passwordHash,
+    role: 'user',
+    status: 'pending',
+    isVerified: true,
+  })
 
-        const savedUser = await user.save()
-            // email verification
-            // jwt.sign(
-            //     { user: savedUser._id },
-            //     process.env.EMAIL_SECRET,
-            //     { expiresIn: '1d' },
-            //     (error, emailToken) => {
-            //         if (error) {
-            //             response.status(400)
-            //         }
-            //         const url = `http://localhost:3001/confirmation/${emailToken}`;
-            //         transporter.sendMail({
-            //             to: "arvedsystem@outlook.com",
-            //             subject: "Confrim Your ARVED account",
-            //             html: `<html>
-            //             <body> To confim your email click here: <a href= "${url}"> ${url}</a>  </body>
-            //             </html>`
-            //         })
-            //     }
-            // )
-        response.status(201).json(savedUser)
-    })
+  const savedUser = await user.save()
+  // email verification
+  // jwt.sign(
+  //     { user: savedUser._id },
+  //     process.env.EMAIL_SECRET,
+  //     { expiresIn: '1d' },
+  //     (error, emailToken) => {
+  //         if (error) {
+  //             response.status(400)
+  //         }
+  //         const url = `http://localhost:3001/confirmation/${emailToken}`;
+  //         transporter.sendMail({
+  //             to: "arvedsystem@outlook.com",
+  //             subject: "Confrim Your ARVED account",
+  //             html: `<html>
+  //             <body> To confim your email click here: <a href= "${url}"> ${url}</a>  </body>
+  //             </html>`
+  //         })
+  //     }
+  // )
+  response.status(201).json(savedUser)
+})
 
 // confirming email
-loginRouter.get('/confirmation/:token',
-    async (request, response) => {
-        jwt.verify(request.params.token, process.env.EMAIL_SECRET, async (err, decoded) => {
-            if (err) {
-               return response.status(401).json({ error: "eroor: email verification failed" })
-            }
-            await User.findByIdAndUpdate(decoded.user, {isVerified: true }, { new: true })
-            response.status(200).redirect('http://localhost:3000/verification')
-        })       
-    })   
+loginRouter.get('/confirmation/:token', async (request, response) => {
+  jwt.verify(
+    request.params.token,
+    process.env.EMAIL_SECRET,
+    async (err, decoded) => {
+      if (err) {
+        return response
+          .status(401)
+          .json({ error: 'eroor: email verification failed' })
+      }
+      await User.findByIdAndUpdate(
+        decoded.user,
+        { isVerified: true },
+        { new: true }
+      )
+      response.status(200).redirect('http://localhost:3000/verification')
+    }
+  )
+})
 
 // for login
-loginRouter.post('/login',
-    async (request, response) => {
+loginRouter.post('/login', async (request, response) => {
+  const { email, password } = request.body
+  const user = await User.findOne({ email: email })
+  const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash)
 
-        const {email, password} = request.body
-        const user = await User.findOne({ email: email })
-        const passwordCorrect = user === null 
-            ? false 
-            : await bcrypt.compare(password, user.passwordHash)
-        
-        if (!(user && passwordCorrect)) {
-            return response.status(401).json({
-                error: 'invalid username or password'
-            })
-        }
-
-        const userForToken = {
-            name: user.name,
-            id: user._id
-        }
-        const token = jwt.sign(userForToken, process.env.SECRET)
-        response
-        .status(200)
-            .send({
-                token,
-                name: user.name,
-                surname: user.surname,
-                status: user.status,
-                isVerified: user.isVerified,
-                role: user.role,
-                department: user.department
-            })
-                // response
-
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password',
     })
+  }
+
+  const userForToken = {
+    name: user.name,
+    id: user._id,
+  }
+  const token = jwt.sign(userForToken, process.env.SECRET)
+  response.status(200).send({
+    token,
+    name: user.name,
+    surname: user.surname,
+    status: user.status,
+    isVerified: user.isVerified,
+    role: user.role,
+    department: user.department,
+  })
+  // response
+})
 
 // for forgot password (takes a user email & creates a link for changing password and sends it that email)
-loginRouter.post('/forgot-password', 
-    async (request, response) => {
-        const {email} = request.body
-        User.findOne({ email: email }, (err, user) => {
-            if (err || !user){
-                return response.status(400).send("user with this email does not exist")
-            }
-            const userToken = jwt.sign(
-                { user: user._id }, 
-                process.env.FORGOT_PASSWORD_SECRET,
-                { expiresIn: '15m' }
-            )
-            
-            const url = `http://localhost:3001/reset-password/${userToken}`;
-            transporter.sendMail({
-                to: "ismailkamaleldin@gmail.com",
-                subject: "change Your ARVED password",
-                html: `To change your password click here: <a href= "${url}"> ${url}</a>`
-            })
-            response.status(200).send({userToken})
-        }) 
+loginRouter.post('/forgot-password', async (request, response) => {
+  const { email } = request.body
+  User.findOne({ email: email }, (err, user) => {
+    if (err || !user) {
+      return response.status(400).send('user with this email does not exist')
+    }
+    const userToken = jwt.sign(
+      { user: user._id },
+      process.env.FORGOT_PASSWORD_SECRET,
+      { expiresIn: '15m' }
+    )
+
+    const url = `http://localhost:3001/reset-password/${userToken}`
+    transporter.sendMail({
+      to: 'ismailkamaleldin@gmail.com',
+      subject: 'change Your ARVED password',
+      html: `To change your password click here: <a href= "${url}"> ${url}</a>`,
     })
+    response.status(200).send({ userToken })
+  })
+})
 
 // for reset password (creates a link and sends it to user email)
-loginRouter.put('/reset-password/:token', 
-    async (request, response) => {
-        const {password} = request.body
-        jwt.verify(request.params.token, process.env.FORGOT_PASSWORD_SECRET, async (err, decoded) => {
-            if (err) {
-               return response.status(401).json({ error: "eroor: token verification failed" })
-            }
-            
-            const saltRounds = 10
-            const passwordHash = await bcrypt.hash(password, saltRounds)
-            await User.findByIdAndUpdate(decoded.user, { passwordHash: passwordHash }, { new: true })
-            response.status(200).send("User password updated")              
-            
-        })  
-    })
+loginRouter.put('/reset-password/:token', async (request, response) => {
+  const { password } = request.body
+  jwt.verify(
+    request.params.token,
+    process.env.FORGOT_PASSWORD_SECRET,
+    async (err, decoded) => {
+      if (err) {
+        return response
+          .status(401)
+          .json({ error: 'eroor: token verification failed' })
+      }
+
+      const saltRounds = 10
+      const passwordHash = await bcrypt.hash(password, saltRounds)
+      await User.findByIdAndUpdate(
+        decoded.user,
+        { passwordHash: passwordHash },
+        { new: true }
+      )
+      response.status(200).send('User password updated')
+    }
+  )
+})
 
 loginRouter.get('/faculties', async (request, response) => {
-    let faculties = await Faculty.find({})
-    response.status(200).json(faculties)
+  let faculties = await Faculty.find({})
+  response.status(200).json(faculties)
 })
 
 // POST is used since can't send payload with get request.
 loginRouter.post('/departments', async (request, response) => {
-    let departments = await Department.find({ faculty: request.body.faculty })
-    response.status(200).json(departments)
+  let departments = await Department.find({ faculty: request.body.faculty })
+  response.status(200).json(departments)
 })
 
-loginRouter.get('/academic_titles', async () => {
-
-})
+loginRouter.get('/academic_titles', async () => {})
 
 module.exports = loginRouter
